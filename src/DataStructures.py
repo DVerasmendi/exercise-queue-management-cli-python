cfaa9b106428b4f344b1c2ee9446a9ea55870948
Deike <deikerv@gmail.com>
2020-03-13 10:41:31 -0300

    avances

--- src/DataStructures.py
+++ src/DataStructures.py
@@ -1,18 +1,79 @@
+import json
+
 class Queue:
 
     def __init__(self, mode, current_queue=[]):
         self._queue = current_queue
+        self._mode = mode 
         # depending on the _mode, the queue has to behave like a FIFO or LIFO
-        if mode is None:
-            raise "Please specify a queue mode FIFO or LIFO"
-        else:
-            self._mode = mode
-    
+        #if mode is None:
+        #    raise "Please specify a queue mode FIFO or LIFO"
+        #else:
+        #    self._mode = mode 
+        
+    ################ ENCOLAR  ##########################
     def enqueue(self, item):
-        pass
+        self._queue.append(item)
+    
+    ################ DESENCOLAR  #######################
     def dequeue(self):
+        try:
+            return self._queue.pop()
+        except:
+            raise ValueError("La cola está vacía")
         pass
+    
+    ################ OBTENER COLA  ##########################
     def get_queue(self):
-        return self._queue
+        
+    ################ COLA-FIFO   ############################
+        if "FIFO" in self._mode:
+            return self._queue
+        
+    ################ COLA-LIFO   ############################
+        elif "LIFO" in self._mode:
+            i=len(self._queue)-1
+            #colaLIFO=[]
+            # for item in range(len(self._queue)):
+            #     #print(self._queue[i])
+            #     colaLIFO.append(self._queue[i])
+            #     i=i-1
+            colaLIFO=[]
+            for item in reversed(self._queue):
+                colaLIFO.append(item)
+            return colaLIFO
+    ################ OBTENER TAMAÑO DE COLA  #################
     def size(self):
+        cola=self._queue
+        cola_size=len(cola)
+        return cola_size
         pass
+    ################ GUARDAR EN ARCHIVO JSON COLA ############
+    def save_json(self):
+        cola=self._queue
+        
+        if "FIFO" in self._mode:
+            with open('clientesFIFO.json', 'w') as file:
+                json.dump(cola, file, indent=1)
+                print(cola)
+            print('\n Archivo creado ---> '+'clientesFIFO.json')
+            
+        elif "LIFO" in self._mode:
+            colaLIFO=[]
+            for item in reversed(self._queue):
+                colaLIFO.append(item)
+    ################ ############### ##########################
+            with open('clientesLIFO.json', 'w') as file:
+                json.dump(colaLIFO, file, indent=1)
+                print(cola)
+            print('\n Archivo creado ---> '+'clientesLIFO.json')
+    ################ LEER ARCHIVO JSON E INCORPORAR COLA  ######
+    def load_json(self):
+        if "FIFO" in self._mode:
+            with open('clientesFIFO.json') as file:
+                self._queue = json.load(file)
+                print('\n Archivo CARGADO FIFO---> '+'clientesFIFO.json')
+        elif "LIFO" in self._mode:
+            with open('clientesLIFO.json') as file:
+                self._queue = json.load(file)
+                print('\n Archivo CARGADO LIFO---> '+'clientesLIFO.json')
--- src/app.py
+++ src/app.py
@@ -1,26 +1,39 @@
 import json
 from DataStructures import Queue
-from sms import send
+#from sms import send
 
 # there queue has to be declared globally (outside any other function)
 # that way all methods have access to it
-queue = Queue(mode="FIFO")
-    
+#queue = Queue(mode="FIFO")
+
 def print_queue():
+    
     # you must print on the console the entire queue list
     print("Printing the entire list...")
     print(queue.get_queue())
 
-def add():
+def add(item):
+    #print(item)
+    #agregando=Queue()
+    agregando=queue.enqueue(item)
+    #print(agregando)
     pass
 
 def dequeue():
+    eliminando=queue.dequeue()
+    pass
+
+def sizequeue():
+    sizeCola=queue.size()
+    print(sizeCola)
     pass
 
 def save():
+    cola_json=queue.save_json()
     pass
 
 def load():
+    load_cola_json=queue.load_json()
     pass 
         
     
@@ -30,19 +43,64 @@ while stop == False:
     
     print('''
 What would you like to do (type a number and press Enter)?
-- Type 1: For adding someone to the Queue.
-- Type 2: For removing someone from the Queue.
-- Type 3: For printing the current Queue state.
-- Type 4: To export the queue to the queue.json file.
-- Type 5: To import the queue from the queue.json file.
-- Type 6: To quit
+- Type 1: Que tipo de cola quiere crear?
+- Type 2: For adding someone to the Queue.
+- Type 3: For removing someone from the Queue.
+- Type 4: For printing the current Queue state.
+- Type 5: To export the queue to the queue.json file.
+- Type 6: To import the queue from the queue.json file.
+- Type 7: Cola Size.
+- Type 8: To quit
     ''')
 
     option = int(input("Enter a number:"))
-    # add your options here using conditionals (if)
-    if option == 3:
+    # add your options here using conditionals (if)     
+    if option == 1:
+        Tipo=input("\n1.-FIFO \n 2.-LIFO \n")
+        if "1" in Tipo:
+            Tipo="FIFO"
+            queue = Queue(mode=Tipo)
+        elif "2" in Tipo:
+            Tipo="LIFO"
+            queue = Queue(mode=Tipo)
+        else:
+            Tipo="FIFO"
+            queue = Queue(mode=Tipo)
+            
+    elif option == 2:
+        user=input("Agregue el nombre del usuario a la cola: ")
+        add(user)      
+     
+    elif option == 3:
+        dequeue()
+    
+    elif option == 4:
         print_queue()
+    
+    elif option == 5:
+        save()
+    
     elif option == 6:
+        Tipo=input("Que tipo de cola desea cargar?: \n 1.-FIFO \n 2.-LIFO\n")
+        if "1" in Tipo:
+            Tipo="FIFO"
+            queue = Queue(mode=Tipo)
+            load()
+
+        elif "2" in Tipo:
+            Tipo="LIFO"
+            queue = Queue(mode=Tipo)
+            load()
+       
+        else:
+            Tipo="FIFO"
+            queue = Queue(mode=Tipo)
+            load()
+        
+    elif option == 7:
+        sizequeue()
+                       
+    elif option == 8:
         print("Bye bye!")
         stop = True
     else:
